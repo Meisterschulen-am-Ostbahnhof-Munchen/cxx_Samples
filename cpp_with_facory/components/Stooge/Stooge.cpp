8b8d7f17f97f276e91fcab9cffe02cdf6ef7a77a
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-24 10:54:11 +0200

    raw copy ...

--- cpp_with_facory/components/Stooge/Stooge.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "Stooge.h"
-
-void func(void)
-{
-
-}
--- cpp_with_facory/components/Stooge/include/Stooge.h
+++ cpp_with_facory/components/Stooge/include/Stooge.h
@@ -1,1 +1,22 @@
-void func(void);
+/*
+ * Stooge.h
+ *
+ *  Created on: 24.07.2021
+ *      Author: FranzHoepfinger
+ */
+
+#ifndef COMPONENTS_STOOGE_INCLUDE_STOOGE_H_
+#define COMPONENTS_STOOGE_INCLUDE_STOOGE_H_
+
+
+class Stooge
+{
+  public:
+	// Empty virtual destructor for proper cleanup
+	virtual ~Stooge() {}
+
+    virtual void slap_stick() = 0;
+};
+
+
+#endif /* COMPONENTS_STOOGE_INCLUDE_STOOGE_H_ */
