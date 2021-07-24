acb9f436b68133a4e5bdd332eddde83de54be712
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-07-24 11:35:30 +0200

    Build complete (1 errors, 0 warnings)

---
+++ cpp_components_with_facory/components/User/include/User.h
@@ -0,0 +1,18 @@
+/*
+ * User.h
+ *
+ *  Created on: 24.07.2021
+ *      Author: FranzHoepfinger
+ */
+
+#ifndef COMPONENTS_USER_INCLUDE_USER_H_
+#define COMPONENTS_USER_INCLUDE_USER_H_
+
+
+
+void user(void);
+
+
+
+
+#endif /* COMPONENTS_USER_INCLUDE_USER_H_ */
--- cpp_components_with_facory/main/main.cpp
+++ cpp_components_with_facory/main/main.cpp
@@ -1,25 +1,4 @@
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <algorithm>
-#include <cxxabi.h>
-
-using std::cout;
-using std::endl;
-using std::string;
-using std::shared_ptr;
-using std::make_shared;
-using std::vector;
-
-
-#include "Stooge.h"
-#include "StoogeFactory.h"
-
-/* these 3 Includes are no more needed. !! and THIS is the Advantage !*/
-/* because they are now in "priv_include" they would not be ABLE to be included ! */
-//NOT #include "Larry.h"
-//NOT #include "Moe.h"
-//NOT #include "Curly.h"
+#include "User.h"
 
 
 /* Inside .cpp file, app_main function must be declared with C linkage */
@@ -27,22 +6,8 @@ extern "C" void app_main(void)
 {
 
 
-	 vector<Stooge*> roles;
+	user();
 
 
-     cout << "Example cpp_with_facory." << endl;
-
-
-     roles.push_back(StoogeFactory::make_stooge(cLarry));
-     roles.push_back(StoogeFactory::make_stooge(cMoe));
-     roles.push_back(StoogeFactory::make_stooge(cCurly));
-
-     for (int i = 0; i < roles.size(); i++)
-       roles[i]->slap_stick();
-     for (int i = 0; i < roles.size(); i++)
-       delete roles[i];
-
-
-     cout << "Example finished." << endl;
 }
 
