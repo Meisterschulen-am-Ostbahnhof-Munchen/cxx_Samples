ab328ddccfb7d22b44e5ffb650656e07585a9d15
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-08-12 21:28:18 +0200

    WORKS

--- qsort/main/main.c
+++ qsort/main/main.c
@@ -13,9 +13,36 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+
+
+
+int values[] = { 88, 56, 100, 2, 25 };
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+
+
+
 void app_main(void)
 {
     printf("Hello world!\n");
 
 
+
+
+    int n;
+
+    printf("Before sorting the list is: \n");
+    for( n = 0 ; n < 5; n++ ) {
+       printf("%d ", values[n]);
+    }
+
+    qsort(values, 5, sizeof(int), cmpfunc);
+
+    printf("\nAfter sorting the list is: \n");
+    for( n = 0 ; n < 5; n++ ) {
+       printf("%d ", values[n]);
+    }
 }
