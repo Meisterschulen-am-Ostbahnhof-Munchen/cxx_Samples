49513fb1db0534660253c650c8f21c4ac1eb9506
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-08-12 11:53:49 +0200

    clean up

---
+++ UsingMemberFunctionsWithQSort/main/main.cpp
@@ -0,0 +1,25 @@
+/* C++ run-time type info (RTTI) example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+
+/* Inside a .cpp file, app_main function must be declared with C linkage */
+extern "C" void app_main()
+{
+
+    cout << "Example finished." << endl;
+}
