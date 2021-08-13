5247bb97a4bcc042cb9c2ae64fcc5d1c0304715a
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-08-13 11:56:47 +0200

    2 Tasks so far, not yet running.
    Build complete (0 errors, 0 warnings)

--- pthread_OO/components/Task1/src/Task1.cpp
+++ pthread_OO/components/Task1/src/Task1.cpp
@@ -4,7 +4,34 @@
  *  Created on: 13.08.2021
  *      Author: FranzHoepfinger
  */
+#include "Task1.h"
 
 
+namespace AppTask
+{
+    Task1::Task1()
+        : AbstractTask()
+    {
+    }
 
+    Task1::~Task1()
+    {
+    }
 
+    bool Task1::runInit()
+    {
+        return true;
+    }
+
+    bool Task1::runAppInit()
+    {
+        return true;
+    }
+
+    bool Task1::runCyclic()
+    {
+
+        return true;
+    }
+
+} /* namespace AppTask */
