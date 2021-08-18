67ad88397da8faf8cdabf6e1a5404f713e96f396
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-08-18 09:46:51 +0200

    not working yet.

--- functional_programming2/main/main.cpp
+++ functional_programming2/main/main.cpp
@@ -1,7 +1,32 @@
 
 
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+
+using namespace std;
+
+
+class MyLess
+{
+public:
+   bool operator()(int a, int b)
+   {
+	   return a < b;
+   }
+};
+
 extern "C" void app_main(void)
 {
+   vector<int> v{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
 
+   sort(begin(v), end(v), MyLess());
+
+   cout << " Hi ";
+
+   for (auto a : v) {
+       cout << a << ' ';
+   }
 }
-
